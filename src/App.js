d98108f0c09cc0dcab0e2344a905f8cc78546683
Ryan Butler <b6utle5r@gmail.com>
2019-12-14 09:17:15 -0700

    yo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World, it's me
+          Hello World, it's me.
         </p>
         <a
           className="App-link"
