3b49ffb32225e29c752090c73ac9ee7b38e4966b
Ryan Butler <b6utle5r@gmail.com>
2019-12-19 13:27:12 -0700

    css/search

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import Header from '../Header/Header';
-import './Home.css'
+import './Home.css';
+import Search from '../Search/Search';
 
 class Home extends Component {
+
   render(){
     return (
     <div className="Home-main">
@@ -10,8 +12,9 @@ class Home extends Component {
       <div className="Home-title">
        <h1>Free Utah Basement Quote</h1>
        <p>Thinking of finishing your basement? Not sure where to go, how long it will take, or how much to budget?</p>
-       <p>Our database of qualified/licensed contracts provide you with accurate, timely quotes to get your job done.</p>
+       <p>Our database of qualified/licensed contracts provide you with accurate, timely quotes to finish your dream basement.</p>
       </div>
+      <Search/>
     </div>
   );
 }
