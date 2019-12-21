9414354783b5538177a2bd369d1aa7fff2eb71da
Ryan Butler <b6utle5r@gmail.com>
2019-12-21 08:29:25 -0700

    added css to searchbar

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -32,14 +32,17 @@ class Search extends Component {
    <form onSubmit={this.handleSubmit.bind(this)} method="POST">
     <div className="form-group">
         <label>Name </label>
+        <br/>
         <input type="text" className="form-control" value={this.state.name} onChange={this.onNameChange.bind(this)} />
     </div>
     <div className="form-group">
         <label>Email </label>
+        <br/>
         <input type="email" className="form-control" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
     </div>
     <div className="form-group">
         <label>Phone </label>
+        <br/>
         <input type="tel" className="form-control" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
     </div>
     <button type="submit" className="btn btn-primary">Submit</button>
